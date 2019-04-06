054662bb05a000f869f0586809a5b9cceab1a537
7c00 <i@7c00.cc>
2019-04-07 01:21:23 +0800

    Add docs

---
+++ website/siteConfig.js
@@ -0,0 +1,7 @@
+module.exports = {
+  title: "RongCloud React Native IMLib",
+  baseUrl: "/",
+  headerLinks: [],
+  colors: { primaryColor: "#2e8555", secondaryColor: "#205c3b" },
+  highlight: { theme: "default" }
+};
